d7a9202aab04ce78740fad4e2b2abb5276735256
Lucky <lucky123@gmail.com>
2025-04-06 19:05:00 +0530

    github

--- apps/dockly/components/dashboard/settings/servers/welcome-stripe/welcome-suscription.tsx
+++ apps/dockly/components/dashboard/settings/servers/welcome-stripe/welcome-suscription.tsx
@@ -336,7 +336,7 @@ export const WelcomeSuscription = () => {
 												</Button>
 												<Button className="rounded-full  w-fit">
 													<Link
-														href="https://github.com/Dokploy/dokploy"
+														href="https://github.com/ezeslucky/deployit.git"
 														aria-label="Dokploy on GitHub"
 														target="_blank"
 														className="flex flex-row items-center gap-2 "
