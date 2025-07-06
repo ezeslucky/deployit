fd0d2a6a3094e8c0b58c55e4e3119deb8e548b49
Lucky <lucky123@gmail.com>
2025-07-06 22:28:32 +0530

    .docker , license and  many more adddition

--- apps/deployi/drizzle/0003_square_lightspeed.sql
+++ apps/deployi/drizzle/0003_square_lightspeed.sql
@@ -1,1 +1,2 @@
-ALTER TABLE "user" ADD COLUMN "canAccessToDocker" boolean DEFAULT false NOT NULL;
+-- ALTER TABLE "user" ADD COLUMN "canAccessToDocker" boolean DEFAULT false NOT NULL;
+ALTER TABLE [user] ADD canAccessToDocker bit NOT NULL DEFAULT 0;
