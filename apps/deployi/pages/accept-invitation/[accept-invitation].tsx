5dc0d42e2ab7c5b2d4d036baaf7ab9885b54c71b
Aurobindo Patra <lucky@LUCKY.localdomain>
2025-07-07 21:12:36 +0530

    admin is complered

--- apps/deployi/pages/accept-invitation/[accept-invitation].tsx
+++ apps/deployi/pages/accept-invitation/[accept-invitation].tsx
@@ -1,30 +1,36 @@
+import { GetServerSideProps } from "next";
 import { Button } from "@/components/ui/button";
 import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/router";
 
-export const AcceptInvitation = () => {
-	const { query } = useRouter();
+interface AcceptInvitationPageProps {
+  invitationId: string;
+}
 
-	const invitationId = query["accept-invitation"];
+export default function AcceptInvitationPage({ invitationId }: AcceptInvitationPageProps) {
+  const handleAccept = async () => {
+    const result = await authClient.organization.acceptInvitation({ invitationId });
+    console.log(result);
+  };
 
-	// const { data: organization } = api.organization.getById.useQuery({
-	//     id: id as string
-	// })
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button onClick={handleAccept}>Accept Invitation</Button>
+    </div>
+  );
+}
 
-	return (
-		<div>
-			<Button
-				onClick={async () => {
-					const result = await authClient.organization.acceptInvitation({
-						invitationId: invitationId as string,
-					});
-					console.log(result);
-				}}
-			>
-				Accept Invitation
-			</Button>
-		</div>
-	);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const invitationId = context.params?.["accept-invitation"];
+
+  if (!invitationId || typeof invitationId !== "string") {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      invitationId,
+    },
+  };
 };
-
-export default AcceptInvitation;
